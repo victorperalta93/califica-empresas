ddf02c99fb8d552e31b2a627e520bd1410cc32aa
Víctor Peralta <victorperalta.correo@gmail.com>
2019-10-12 04:04:53 +0200

    añade add y delete de calificaciones y empresas (sqlite)

--- model/calificaciones.js
+++ model/calificaciones.js
@@ -13,9 +13,18 @@ class Calificaciones{
         )`
     }
 
-    addCalificacion(usuario,empresa,valor){
+    add(usuario,empresa,valor){
         return this.db.conn.run(`INSERT INTO calificaciones (usuario,empresa,valor) 
                                 VALUES (?,?,?)`,[usuario,empresa,valor])
+    }
+
+    delete(id){
+        return this.db.conn.run('DELETE FROM calificaciones WHERE id=?',
+                                [id])
+    }
+
+    getAll(){
+        return this.db.conn.all('SELECT * FROM calificaciones')
     }
 }
 
--- model/empresas.js
+++ model/empresas.js
@@ -12,9 +12,18 @@ class Empresas{
         return this.db.conn.run(sql)
     }
 
-    addEmpresa(nombre){
+    add(nombre){
         return this.db.conn.run(`INSERT INTO empresas (nombre) VALUES (?)`
                                 ,[nombre])
+    }
+
+    delete(nombre){
+        return this.db.conn.run('DELETE FROM empresas WHERE nombre=?',
+                                [nombre])
+    }
+
+    getAll(){
+        return this.db.conn.all('SELECT * FROM empresas')
     }
 }
 

5eafb7c43228f51517178d03760c75df37d4edfa
Víctor Peralta <victorperalta.correo@gmail.com>
2019-10-12 03:46:44 +0200

    crea conexion a bd (sqlite3)

---
+++ model/db.js
@@ -0,0 +1,14 @@
+import sqlite3 from 'sqlite3';
+
+class DB{
+    constructor(dbPath){
+        this.conn = new sqlite3.Database(dbPath,(err) =>{
+            if(err)
+                console.log('No se pudo conectar a la base de datos.',err)
+            else
+                console.log('Conexión establecida con la base de datos.')
+        });
+    }
+}
+
+module.exports = DB
